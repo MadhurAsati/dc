5f46297513734f6218471d8b70585aefac5aa4e1
MadhurAsati <madhur9827@gmail.com>
2022-06-21 19:01:24 +0530

    Add files via upload

---
+++ DC.ps1
@@ -0,0 +1,8 @@
+﻿[string]$cred = "Happy@123"
+$SPNSecret = ConvertTo-SecureString -String $cred -AsPlainText -Force
+Write-Host "Adding Windo Features"
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Write-Host "Testing ADDSfortst"
+Test-ADDSForestInstallation -DomainName itingredients.com -InstallDns -SafeModeAdministratorPassword $SPNSecret
+Write-Host "Installating ADDSfortst"
+Install-ADDSForest -DomainName itingredients.com -InstallDNS -SafeModeAdministratorPassword $SPNSecret -Force
